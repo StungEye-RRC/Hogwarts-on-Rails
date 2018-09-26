573eadb3a01376f64018f895d73fccbcab713cc5
Kyle Geske <stungeye@gmail.com>
2018-09-26 09:19:50 -0500

    New students controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'students/index'
+  get 'students/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
