a013d24347b6fc8a298e205321687c4d76061a9f
Kyle Geske <stungeye@gmail.com>
2018-09-26 10:47:21 -0500

    Explained RESTful routes. Added some link_tos in the views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  # HTTP GET /students
-  # runs students controller index action
-  get 'students', to: 'students#index'
+  resources :students, only: [:index, :show]
+
+  # RESTful Routing
+  # index     GET    /students
+  # show      GET    /students/:id
+  # new       GET    /students/new
+  # create    POST   /students
+  # edit      GET    /students/:id/edit
+  # update    PUT    /students/:id
+  # update    PATCH  /students/:id
+  # destroy   DELETE /students/:id
+
+  # THE standard DSL:
+  # get 'path', to: 'controller#action'
 
   # HTTP GET /
   # also runs students controller index action
   root to: 'students#index'
-
-  # HTTP GET /students/:id
-  # runs students controller show action
-  get 'students/:id', to: 'students#show', id: /\d+/
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
