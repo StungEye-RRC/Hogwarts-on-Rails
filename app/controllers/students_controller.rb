c85d30b082df038a647e10bed175d94a72d9b957
Kyle Geske <stungeye@gmail.com>
2018-09-26 09:54:51 -0500

    Implemented the students#index action and view.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,19 @@
 class StudentsController < ApplicationController
-  def index
-  end
+  # The methods in a controller tend to be called
+  # actions.
 
+  # The index action
+  def index
+    @students = Student.all
+  end
+  # Automagically renders the
+  # app/views/students/index.html.erb
+  # it will also give the view access to it's
+  # instance variables.
+
+  # The show action
   def show
   end
+  # Automagically renders the
+  # app/views/students/show.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get 'students/index'
+  # HTTP GET /students
+  # runs students controller index action
+  get 'students', to: 'students#index'
+
+  # HTTP GET /
+  # also runs students controller index action
+  root to: 'students#index'
+
+  # HTTP GET /students/show
+  # runs students controller show action
   get 'students/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
